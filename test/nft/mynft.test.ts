ef4e538ad959e1113782f8babcecd3d92f4899a6
calpa <calpaliu@gmail.com>
2024-03-05 23:20:29 +0800

    feat: sbt

--- test/nft/mynft.test.ts
+++ test/nft/mynft.test.ts
@@ -6,14 +6,17 @@ describe("MyNFT", function () {
   let nftContract: Contract;
   let ownerWallet: Wallet;
   let recipientWallet: Wallet;
+  let otherWallet: Wallet;
 
   before(async function () {
     ownerWallet = getWallet(LOCAL_RICH_WALLETS[0].privateKey);
     recipientWallet = getWallet(LOCAL_RICH_WALLETS[1].privateKey);
+    otherWallet = getWallet(LOCAL_RICH_WALLETS[3].privateKey);
 
     nftContract = await deployContract(
       "MyNFT",
-      ["MyNFTName", "MNFT", "https://mybaseuri.com/token/"],
+      [],
+      // ["MyNFTName", "MNFT", "https://mybaseuri.com/token/"],
       { wallet: ownerWallet, silent: true }
     );
   });
@@ -25,20 +28,20 @@ describe("MyNFT", function () {
     expect(balance).to.equal(BigInt("1"));
   });
 
-  it("Should have correct token URI after minting", async function () {
-    const tokenId = 1; // Assuming the first token minted has ID 1
-    const tokenURI = await nftContract.tokenURI(tokenId);
-    expect(tokenURI).to.equal("https://mybaseuri.com/token/1");
-  });
+  // it("Should have correct token URI after minting", async function () {
+  //   const tokenId = 1; // Assuming the first token minted has ID 1
+  //   const tokenURI = await nftContract.tokenURI(tokenId);
+  //   expect(tokenURI).to.equal("https://mybaseuri.com/token/1");
+  // });
 
-  it("Should allow owner to mint multiple NFTs", async function () {
-    const tx1 = await nftContract.mint(recipientWallet.address);
-    await tx1.wait();
-    const tx2 = await nftContract.mint(recipientWallet.address);
-    await tx2.wait();
-    const balance = await nftContract.balanceOf(recipientWallet.address);
-    expect(balance).to.equal(BigInt("3")); // 1 initial nft + 2 minted
-  });
+  // it("Should allow owner to mint multiple NFTs", async function () {
+  //   const tx1 = await nftContract.mint(recipientWallet.address);
+  //   await tx1.wait();
+  //   const tx2 = await nftContract.mint(recipientWallet.address);
+  //   await tx2.wait();
+  //   const balance = await nftContract.balanceOf(recipientWallet.address);
+  //   expect(balance).to.equal(BigInt("3")); // 1 initial nft + 2 minted
+  // });
 
   it("Should not allow non-owner to mint NFTs", async function () {
     try {
@@ -47,6 +50,20 @@ describe("MyNFT", function () {
       expect.fail("Expected mint to revert, but it didn't");
     } catch (error) {
       expect(error.message).to.include("Ownable: caller is not the owner");
+    }
+  });
+
+  it("Should not be able to transfer NFT", async function () {
+    try {
+      const tx = await nftContract.mint(recipientWallet.address);
+      await tx.wait();
+      // const balance = await nftContract.balanceOf(recipientWallet.address);
+      // expect(balance).to.equal(BigInt("2"));
+
+      const tx2 = await (nftContract.connect(recipientWallet) as Contract).transferFrom(recipientWallet.address, otherWallet.address, 1);
+      await tx2.wait();
+    } catch (error) {
+      expect(error.message).to.include("Token not transferable");
     }
   });
 });
