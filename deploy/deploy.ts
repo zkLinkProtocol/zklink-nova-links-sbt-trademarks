047d3bda4d45afad26aaeaf9bf5d5f599a3a5239
calpa <calpaliu@gmail.com>
2024-03-05 22:17:13 +0800

    feat: init

---
+++ deploy/deploy.ts
@@ -0,0 +1,13 @@
+import { deployContract } from "./utils";
+
+// An example of a basic deploy script
+// It will deploy a Greeter contract to selected network
+// as well as verify it on Block Explorer if possible for the network
+export default async function () {
+  console.log("Deploying Basic Contract");
+  const contractArtifactName = "Greeter";
+  const constructorArguments = ["Hi there!"];
+  await deployContract(contractArtifactName, constructorArguments, {
+    noVerify: true,
+  });
+}
