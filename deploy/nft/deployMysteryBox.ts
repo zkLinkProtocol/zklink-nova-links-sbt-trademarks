68cd8c55a923fc4d5d822c1e4af186a06fed49a2
Calpa Liu <calpaliu@gmail.com>
2024-03-11 12:26:02 +0800

    feat: add box nft

---
+++ deploy/nft/deployMysteryBox.ts
@@ -0,0 +1,7 @@
+import { deployContract } from "../utils";
+
+export default async function () {
+  await deployContract("MysteryBoxNFT", [process.env.WITNESS_ADDRESS], {
+    noVerify: false
+  });
+}
