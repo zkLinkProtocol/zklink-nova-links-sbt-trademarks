a2244d10a07ee783147f15833d68d3cfc2646d9a
zkbenny <benny@zklink.org>
2024-03-21 11:40:40 +0800

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,9 +4,6 @@ import "@matterlabs/hardhat-zksync-node";
 import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
 import "@matterlabs/hardhat-zksync-verify";
-import "@matterlabs/hardhat-zksync-upgradable";
-
-import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "zklinkNovaGoerliTestnet",
