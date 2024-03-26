e7a9f5e3ecc0966896496b071ae41057c5f013a6
chuhe <3568001975@qq.com>
2024-03-26 13:11:09 +0800

    update test code

--- test/nft/NovaLynksNFT.test.ts
+++ test/nft/NovaLynksNFT.test.ts
@@ -2,7 +2,6 @@ import { expect, assert } from 'chai';
 const { ethers } = require('hardhat');
 import { Contract, Wallet } from 'ethers';
 import { upgrades } from 'hardhat';
-import { keccak256 } from "@ethersproject/keccak256";
 
 describe('NovaLynksNFT', function () {
   let NovaLynksNFT;
@@ -29,10 +28,10 @@ describe('NovaLynksNFT', function () {
     console.log('addr1:', addr1.address);
     console.log('addr2:', addr2.address);
 
-    Mystery = await upgrades.deployProxy(NovaMysteryBoxNFT,["NMB", "NMB", '', owner.address],{
-        kind: 'uups',
-        initializer: 'initialize',
-        unsafeAllow:["constructor"]
+    Mystery = await upgrades.deployProxy(NovaMysteryBoxNFT, ['NMB', 'NMB', '', owner.address], {
+      kind: 'uups',
+      initializer: 'initialize',
+      unsafeAllow: ['constructor'],
     });
 
     mysteryAddr = await Mystery.getAddress();
@@ -55,7 +54,7 @@ describe('NovaLynksNFT', function () {
     lynkAddr = await Lynk.getAddress();
   });
 
-  it('batch mint TradeMark error', async function () {
+  it('mint TradeMark error', async function () {
     tradeAddr = await TradeMark.getAddress();
     const domain = {
       name: 'TradeMark',
@@ -73,7 +72,7 @@ describe('NovaLynksNFT', function () {
         { name: 'expiry', type: 'uint256' },
       ],
     };
-    
+
     let message1 = {
       to: addr1.address,
       nonce: 1,
@@ -96,15 +95,15 @@ describe('NovaLynksNFT', function () {
       expiry: 1742630631000,
     };
     let message4 = {
-        to: addr1.address,
-        nonce: 1,
-        tokenId: 4,
-        amount: 1,
-        expiry: 1742630631000,
-      };
+      to: addr1.address,
+      nonce: 1,
+      tokenId: 4,
+      amount: 1,
+      expiry: 1742630631000,
+    };
 
     // mint TradeMark
-    
+
     signature = await owner.signTypedData(domain, types, message1);
     await TradeMark['safeMint(address,uint256,uint256,uint256,uint256,bytes)'](
       addr1.address,
@@ -142,58 +141,71 @@ describe('NovaLynksNFT', function () {
       signature,
     );
 
-   
     let token1Bal = await TradeMark.balanceOf(addr1.address, 1);
     let token2Bal = await TradeMark.balanceOf(addr1.address, 2);
     let token3Bal = await TradeMark.balanceOf(addr1.address, 3);
     let token4Bal = await TradeMark.balanceOf(addr1.address, 4);
-    
-    console.log('addr1 token1 balance before:', token1Bal.toString());
-    console.log('addr1 token2 balance before:', token2Bal.toString());
-    console.log('addr1 token3 balance before:', token3Bal.toString());
-    console.log('addr1 token4 balance before:', token4Bal.toString());
+    expect(token1Bal).to.equal(1);
+    expect(token2Bal).to.equal(1);
+    expect(token3Bal).to.equal(1);
+    expect(token4Bal).to.equal(1);
+  });
+
+  it('mint Lynk without SBT error', async function () {
+    lynkAddr = await Lynk.getAddress();
+
+    //  set tokenIds
+    let tokenids: number[] = [1, 2, 3, 4];
+    await Lynk.setTrademarkTokenIds(tokenids);
+
+    // 将token approve给Lynk
+    const addr1TradeMark = new ethers.Contract(await TradeMark.getAddress(), TradeMark.interface, addr1);
+    addr1TradeMark.setApprovalForAll(Lynk.getAddress(), true);
+
+    // mint Lynk
+    const addr1Lynk = new ethers.Contract(await Lynk.getAddress(), Lynk.interface, addr1);
+    await expect(addr1Lynk['safeMint(address)'](addr1)).to.be.revertedWith('Only nova SBT holders can mint');
   });
 
   it('mint Mystery', async function () {
     mysteryAddr = await Mystery.getAddress();
     const domain = {
-        name: "NMB",
-        version: "0",
-        chainId: 31337, 
-        verifyingContract: mysteryAddr,
+      name: 'NMB',
+      version: '0',
+      chainId: 31337,
+      verifyingContract: mysteryAddr,
     };
     let types = {
-        MintAuth: [
-            { name: "to", type: "address" },
-            { name: "nonce", type: "uint256" },
-            { name: "expiry", type: "uint256" },
-        ]
+      MintAuth: [
+        { name: 'to', type: 'address' },
+        { name: 'nonce', type: 'uint256' },
+        { name: 'expiry', type: 'uint256' },
+      ],
     };
 
     let message = {
-        to: addr1.address,
-        nonce: 1,
-        expiry: 1742630631000
+      to: addr1.address,
+      nonce: 1,
+      expiry: 1742630631000,
     };
-    signature = await owner.signTypedData(domain,types,message);
-    // signature = await addr1.signTypedData(domain,types,message); // 报错addr1没有签名权限
-    console.log("signature:", signature);
+    signature = await owner.signTypedData(domain, types, message);
     await Mystery['safeMint(address,uint256,uint256,bytes)'](addr1.address, 1, 1742630631000, signature);
+    expect(await Mystery.balanceOf(addr1.address)).to.equal(1);
   });
 
-  it('mintbatch Lynk', async function () {
+  it('mintbatch TradeMark', async function () {
     tradeAddr = await TradeMark.getAddress();
 
     let tokenIdList: number[] = [1, 2, 3, 4];
     let amountList: number[] = [2, 2, 2, 2];
     const domain = {
-        name: 'TradeMark',
-        version: '0',
-        chainId: 31337,
-        verifyingContract: tradeAddr,
-      };
+      name: 'TradeMark',
+      version: '0',
+      chainId: 31337,
+      verifyingContract: tradeAddr,
+    };
     let batchTypes = {
-       BatchMintAuth: [
+      BatchMintAuth: [
         { name: 'to', type: 'address' },
         { name: 'nonce', type: 'uint256' },
         { name: 'tokenIds', type: 'uint256[]' },
@@ -218,9 +230,11 @@ describe('NovaLynksNFT', function () {
       1742630631000,
       signature,
     );
-  })
+    let token1Bal = await TradeMark.balanceOf(addr1.address, 1);
+    expect(token1Bal).to.equal(3);
+  });
 
-  it(' mint Lynk error', async function () {
+  it('mint Lynk error', async function () {
     lynkAddr = await Lynk.getAddress();
 
     //  set tokenIds
@@ -229,59 +243,71 @@ describe('NovaLynksNFT', function () {
 
     // 将token approve给Lynk
     const addr1TradeMark = new ethers.Contract(await TradeMark.getAddress(), TradeMark.interface, addr1);
-    addr1TradeMark.setApprovalForAll(Lynk.getAddress(),true);
+    addr1TradeMark.setApprovalForAll(Lynk.getAddress(), true);
 
     // mint Lynk
     const addr1Lynk = new ethers.Contract(await Lynk.getAddress(), Lynk.interface, addr1);
     await addr1Lynk['safeMint(address)'](addr1);
-   
+
     const balance = await Lynk.balanceOf(addr1.address);
-    console.log("Lynk balance:", balance); // mint success
+    console.log('Lynk balance:', balance); // mint success
 
     let token1Bal = await TradeMark.balanceOf(addr1.address, 1);
     let token2Bal = await TradeMark.balanceOf(addr1.address, 2);
     let token3Bal = await TradeMark.balanceOf(addr1.address, 3);
     let token4Bal = await TradeMark.balanceOf(addr1.address, 4);
-    console.log('addr1 token1 balance after:', token1Bal.toString());
-    console.log('addr1 token2 balance after:', token2Bal.toString());
-    console.log('addr1 token3 balance after:', token3Bal.toString());
-    console.log('addr1 token4 balance after:', token4Bal.toString());
+    expect(token1Bal).to.equal(2);
+    expect(token2Bal).to.equal(2);
+    expect(token3Bal).to.equal(2);
+    expect(token4Bal).to.equal(2);
   });
 
   it('mint Lynk with Auth error', async function () {
     lynkAddr = await Lynk.getAddress();
-      const domain = {
-        name: "Lynk",
-        version: "0",
-        chainId: 31337, 
-        verifyingContract: lynkAddr,
+    const domain = {
+      name: 'Lynk',
+      version: '0',
+      chainId: 31337,
+      verifyingContract: lynkAddr,
     };
     let types = {
-        MintAuth: [
-            { name: "to", type: "address" },
-            { name: "nonce", type: "uint256" },
-            { name: "expiry", type: "uint256" },
-        ]
+      MintAuth: [
+        { name: 'to', type: 'address' },
+        { name: 'nonce', type: 'uint256' },
+        { name: 'expiry', type: 'uint256' },
+      ],
     };
 
     let message = {
-        to: addr1.address,
-        nonce: 1,
-        expiry: 1742630631000
+      to: addr1.address,
+      nonce: 1,
+      expiry: 1742630631000,
     };
-    signature = await owner.signTypedData(domain,types,message);
-    await Lynk['safeMintWithAuth(address,uint256,uint256,bytes)'](addr1.address,1,1742630631000,signature);
+    signature = await owner.signTypedData(domain, types, message);
+    await Lynk['safeMintWithAuth(address,uint256,uint256,bytes)'](addr1.address, 1, 1742630631000, signature);
     const balance = await Lynk.balanceOf(addr1.address);
-    console.log("Lynk balance Auth:", balance); // mint success
-  })
+    console.log('Lynk balance Auth:', balance); // mint success
+    expect(balance).to.equal(2);
+  });
 
-  it('1155 burn fail', async function () {
+  it('1155 transfer success', async function () {
+    const addr1TradeMark = new ethers.Contract(await TradeMark.getAddress(), TradeMark.interface, addr1);
+    await addr1TradeMark.safeTransferFrom(addr1.address, addr2.address, 1, 1, '0x');
+    let addr2token1Bal = await TradeMark.balanceOf(addr2.address, 1);
+    let addr1token1Bal = await TradeMark.balanceOf(addr1.address, 1);
+    console.log('addr2token1Bal:', addr2token1Bal);
+    console.log('addr1token1Bal:', addr1token1Bal);
+    expect(addr2token1Bal).to.equal(1);
+    expect(addr1token1Bal).to.equal(1);
+  });
+
+  it('1155 burn success', async function () {
     const addr1TradeMark = new ethers.Contract(await TradeMark.getAddress(), TradeMark.interface, addr1);
     const addr2TradeMark = new ethers.Contract(await TradeMark.getAddress(), TradeMark.interface, addr2);
-    addr1TradeMark.setApprovalForAll(addr2.address,true);
-    await addr2TradeMark.burn(addr1.address,1,1);
+    addr1TradeMark.setApprovalForAll(addr2.address, true);
+    await expect(addr2TradeMark.burn(addr1.address, 1, 2)).to.be.revertedWith('ERC1155: burn amount exceeds balance');
+    await addr2TradeMark.burn(addr1.address, 1, 1);
     let token1Bal = await TradeMark.balanceOf(addr1.address, 1);
-    console.log('addr1 token1 balance afterburn:', token1Bal.toString());
-  })
-
+    expect(token1Bal).to.equal(0);
+  });
 });
--- test/nft/NovaMysteryBoxNFT.test.ts
+++ test/nft/NovaMysteryBoxNFT.test.ts
@@ -1,101 +1,125 @@
 import { expect, assert } from 'chai';
 const { ethers } = require('hardhat');
-import { Contract,Wallet } from 'ethers';
+import { Contract, Wallet } from 'ethers';
 import { upgrades } from 'hardhat';
-import { keccak256 } from "@ethersproject/keccak256";
 
-describe("NovaMysteryBoxNFT", function () {
-    let NovaMysteryBoxNFT;
-    let nft:Contract;
+describe('NovaMysteryBoxNFT', function () {
+  let NovaMysteryBoxNFT;
+  let nft: Contract;
 
-    let owner: Wallet;
-    let addr1: Wallet;
-    let addr2: Wallet;
-    let signature;
-    // let test = new Wallet('0xbb64ec9d1d3b623d1172f7fb01e99512232a8064c403f97cb0978a1d4f100fb1', ethers.provider);
+  let owner: Wallet;
+  let addr1: Wallet;
+  let addr2: Wallet;
+  let signature;
 
+  before(async function () {
+    NovaMysteryBoxNFT = await ethers.getContractFactory('NovaMysteryBoxNFT');
+    [owner, addr1, addr2] = await ethers.getSigners();
+    console.log('owner:', owner.address);
+    console.log('addr1:', addr1.address);
+    console.log('addr2:', addr2.address);
 
-    before(async function () {
-        NovaMysteryBoxNFT = await ethers.getContractFactory("NovaMysteryBoxNFT");
-        [owner, addr1, addr2] = await ethers.getSigners();
-        console.log("owner:", owner.address);
-        console.log("addr1:", addr1.address);
-        console.log("addr2:", addr2.address);
-
-        nft = await upgrades.deployProxy(NovaMysteryBoxNFT,["NMB", "NMB", '', owner.address],{
-            kind: 'uups',
-            initializer: 'initialize',
-            unsafeAllow:["constructor"]
-        });
-
+    nft = await upgrades.deployProxy(NovaMysteryBoxNFT, ['NMB', 'NMB', '', owner.address], {
+      kind: 'uups',
+      initializer: 'initialize',
+      unsafeAllow: ['constructor'],
     });
+  });
+
+  it('should sign and verify EIP-712 signature', async function () {
+    let nftAddr = await nft.getAddress();
+    console.log('nftAddr:', nftAddr);
+    const domain = {
+      name: 'NMB',
+      version: '0',
+      chainId: 31337,
+      verifyingContract: nftAddr,
+    };
+    let types = {
+      MintAuth: [
+        { name: 'to', type: 'address' },
+        { name: 'nonce', type: 'uint256' },
+        { name: 'expiry', type: 'uint256' },
+      ],
+    };
+
+    let message = {
+      to: addr1.address,
+      nonce: 1,
+      expiry: 1742630631000,
+    };
+    signature = await owner.signTypedData(domain, types, message);
+    // signature = await addr1.signTypedData(domain,types,message); // AccessControl error
+    await nft['safeMint(address,uint256,uint256,bytes)'](addr1.address, 1, 1742630631000, signature);
+    const balance = await nft.balanceOf(addr1.address);
+    expect(balance).to.equal(1);
+  });
 
 
-    it("should sign and verify EIP-712 signature", async function () {
-        // 构建 domain 数据
-        let nftAddr = await nft.getAddress();
-        console.log("nftAddr:", nftAddr);
-        const domain = {
-            name: "NMB",
-            version: "0",
-            chainId: 31337, 
-            verifyingContract: nftAddr
-        };
-        let types = {
-            MintAuth: [
-                { name: "to", type: "address" },
-                { name: "nonce", type: "uint256" },
-                { name: "expiry", type: "uint256" },
-            ]
-        };
-    
-        let message = {
-            to: addr1.address,
-            nonce: 1,
-            expiry: 1742630631000
-        };
-        signature = await owner.signTypedData(domain,types,message);
-        // signature = await addr1.signTypedData(domain,types,message); // 报错addr1没有签名权限
-        console.log("signature:", signature);
-        await nft['safeMint(address,uint256,uint256,bytes)'](addr1.address, 1, 1742630631000, signature);
-        const balance = await nft.balanceOf(addr1.address);
-        console.log("balance:", balance);
-    });
+  it('only owner can signature', async function () {
+    let nftAddr = await nft.getAddress();
+    console.log('nftAddr:', nftAddr);
+    const domain = {
+      name: 'NMB',
+      version: '0',
+      chainId: 31337,
+      verifyingContract: nftAddr,
+    };
+    let types = {
+      MintAuth: [
+        { name: 'to', type: 'address' },
+        { name: 'nonce', type: 'uint256' },
+        { name: 'expiry', type: 'uint256' },
+      ],
+    };
 
+    let message = {
+      to: addr1.address,
+      nonce: 1,
+      expiry: 1742630631000,
+    };
+    signature = await addr1.signTypedData(domain,types,message);
+    expect(nft['safeMint(address,uint256,uint256,bytes)'](addr1.address, 1, 1742630631000, signature)).to.be.reverted;
+  })
 
-    /**
-     * test transfer
-     * not owner error: ERC721: transfer from incorrect owner
-     */
-    // it("non-NFT owner should not have permission to transfer", async function () {
-    //     const addr1TokenContract = new ethers.Contract(await nft.getAddress(), nft.interface, addr1);
-    //     const addr2TokenContract = new ethers.Contract(await nft.getAddress(), nft.interface, addr2);
-    //     await addr1TokenContract.approve(addr2.address,0);
-    //     await addr2TokenContract.transferFrom(addr1,addr2,0);  
-    //     assert.equal(await nft.ownerOf(0), addr2.address);
-    // });
+  /**
+   * test transfer
+   */
+  it("non-NFT owner should not have permission to transfer", async function () {
+      const addr1TokenContract = new ethers.Contract(await nft.getAddress(), nft.interface, addr1);
+      const addr2TokenContract = new ethers.Contract(await nft.getAddress(), nft.interface, addr2);
+      await addr1TokenContract.approve(addr2.address,0);
+      await addr2TokenContract.transferFrom(addr1,addr2,0);
+      assert.equal(await nft.ownerOf(0), addr2.address);
+  });
 
-    /**
-     * test not ower or approved burn
-     */
-    // it("non-NFT owner should not have permission to burn", async function () {
-    //     // error message：'ERC721: caller is not token owner or approved'
-    //     const addr1TokenContract = new ethers.Contract(await nft.getAddress(), nft.interface, addr1);
-    //     addr1TokenContract.approve(addr2.address,0);
-    //     const addr2TokenContract = new ethers.Contract(await nft.getAddress(), nft.interface, addr2);
-    //     await addr2TokenContract.burn(0);
-    // });
+  /**
+   * test not ower or approved burn
+   */
+  it("approve burn error", async function () {
+      const addr1TokenContract = new ethers.Contract(await nft.getAddress(), nft.interface, addr1);
+      addr1TokenContract.approve(addr2.address,0);
+      const addr2TokenContract = new ethers.Contract(await nft.getAddress(), nft.interface, addr2);
+      await addr2TokenContract.burn(0);
+      let exist = await addr1TokenContract.exists(0);
+      expect(exist).to.equal(false);
+  });
 
-    /**
-     * test ower burn
-     */
-    it("burn fail", async function () {
-        const addr1TokenContract = new ethers.Contract(await nft.getAddress(), nft.interface, addr1);
-        await addr1TokenContract.burn(0);
-        let exist = await addr1TokenContract.exists(0);
-        console.log("exist:",exist);
+  it('non-NFT owner should not have permission to burn', async function () {
+    const addr2TokenContract = new ethers.Contract(await nft.getAddress(), nft.interface, addr2);
+    await expect( addr2TokenContract.burn(0)).to.be.revertedWith("ERC721: caller is not token owner or approved");
+  });
 
-        let nonce = await addr1TokenContract.burnNonces(addr1.address);
-        console.log("nonce:",nonce);
-    });
+  /**
+   * test ower burn
+   */
+  it("burn fail", async function () {
+      const addr1TokenContract = new ethers.Contract(await nft.getAddress(), nft.interface, addr1);
+      await addr1TokenContract.burn(0);
+      let exist = await addr1TokenContract.exists(0);
+      expect(exist).to.equal(false);
+
+      let nonce = await addr1TokenContract.burnNonces(addr1.address);
+      expect(nonce).to.equal(1);
+  });
 });
