0594b7af387687a0e5e2ac3f89f27ec78b938378
chuhe <3568001975@qq.com>
2024-04-04 16:25:15 +0800

    nft PhaseII

---
+++ deploy/nft/deployNovaMysteryBoxPhaseII.ts
@@ -0,0 +1,20 @@
+import { deployContract } from '../utils';
+
+export default async function () {
+  await deployContract(
+    'NovaMysteryBoxPhaseIINFT',
+    [],
+    {
+      noVerify: false,
+      upgradable: true,
+      kind: 'uups',
+      unsafeAllow: ['constructor'],
+    },
+    [
+      'Nova Mystery Box - Phase II',
+      'MYSTERYBOX II',
+      'ipfs://QmYywhGaPuJmoYw7ab7rxBaFiMn8sYmbkDwWsGYKArx2RN',
+      process.env.WITNESS_ADDRESS,
+    ],
+  );
+}
