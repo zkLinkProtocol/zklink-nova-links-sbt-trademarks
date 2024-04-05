939ad588566fc37fda93b7df5bfde4e1647ffb34
zkJoaquin <joaquin@zklink.org>
2024-04-06 00:15:15 +0800

    update NovaTrademarkNFT upgrade script

--- deploy/nft/upgradeNFTContract.ts
+++ deploy/nft/upgradeNFTContract.ts
@@ -1,9 +1,10 @@
 import { upgradeContract } from '../utils';
 
 export default async function () {
-  await upgradeContract('NovaLynksNFT', [process.env.SBT_CONTRACT_ADDRESS, process.env.TRADEMARK_CONTRACT_ADDRESS], {
+  await upgradeContract('NovaTrademarkNFT',
+    [], {
     noVerify: false,
     upgradable: true,
-    unsafeAllow: ['constructor', 'state-variable-immutable'],
+    unsafeAllow: ['constructor'],
   });
 }
--- deploy/nft/verifyContract.ts
+++ deploy/nft/verifyContract.ts
@@ -1,5 +1,5 @@
 import { verifyContractByName } from '../utils';
 
 export default async function () {
-  await verifyContractByName('', '', []);
+  await verifyContractByName('0xA594bF8Ec851a7c58a348DF81Bb311cE0BCAD5C4', 'NovaTrademarkNFT', []);
 }
