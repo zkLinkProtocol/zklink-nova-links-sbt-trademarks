f1aec25d757774101d8a047ff97277dc5bc76def
Hedy-chu <hechu1213@gmail.com>
2024-09-24 17:11:57 +0800

    cuboIII

---
+++ deploy/nft/deployGenesisPassPhaseIII.ts
@@ -0,0 +1,21 @@
+import { deployContract } from '../utils';
+
+export default async function () {
+  await deployContract(
+    'NovaGenesisPassPhaseIIINFT',
+    [],
+    {
+      noVerify: false,
+      upgradable: true,
+      kind: 'uups',
+      unsafeAllow: ['constructor'],
+    },
+    [
+      'Cubo the Block',
+      'CUBO',
+      'https://zklink-nova-nft.s3.ap-northeast-1.amazonaws.com/cubonft/',
+      100001,
+      process.env.GENESISPASS_PHASEII_WITNESS_ADDRESS,
+    ],
+  );
+}
